f6b7fffb00686721cb00e94b120af40055a8692a
sumitrawal658 <133184340+sumitrawal658@users.noreply.github.com>
2026-06-04 01:21:47 +0700

    Github data added

--- index.js
+++ index.js
@@ -22,3 +22,43 @@ app.get('/youtube',(req,res)=>{
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const githubData ={
+     "login": "hiteshchaudhary",
+  "id": 17576078,
+  "node_id": "MDQ6VXNlcjE3NTc2MDc4",
+  "avatar_url": "https://avatars.githubusercontent.com/u/17576078?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/hiteshchaudhary",
+  "html_url": "https://github.com/hiteshchaudhary",
+  "followers_url": "https://api.github.com/users/hiteshchaudhary/followers",
+  "following_url": "https://api.github.com/users/hiteshchaudhary/following{/other_user}",
+  "gists_url": "https://api.github.com/users/hiteshchaudhary/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/hiteshchaudhary/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/hiteshchaudhary/subscriptions",
+  "organizations_url": "https://api.github.com/users/hiteshchaudhary/orgs",
+  "repos_url": "https://api.github.com/users/hiteshchaudhary/repos",
+  "events_url": "https://api.github.com/users/hiteshchaudhary/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/hiteshchaudhary/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 0,
+  "public_gists": 0,
+  "followers": 7,
+  "following": 0,
+  "created_at": "2016-03-01T18:11:15Z",
+  "updated_at": "2016-03-01T18:11:15Z"
+}
+
+app.get('/github',(req,res)=>{
+    res.json(githubData);
+})
